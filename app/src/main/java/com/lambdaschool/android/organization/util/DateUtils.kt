0457dbe25b8a293b3c476eb2ecaf85e5a2f15382
Justin Gent <justinbgent@gmail.com>
2019-08-08 14:52:57 -0600

    Insert product Data, Finish and add methods, setup UI

--- app/src/main/java/com/lambdaschool/android/organization/util/DateUtils.kt
+++ app/src/main/java/com/lambdaschool/android/organization/util/DateUtils.kt
@@ -1,16 +1,21 @@
-package com.lambdaschool.android.organization
+package com.lambdaschool.android.organization.util
 
 import java.text.SimpleDateFormat
 import java.util.*
 
 class DateUtils {
     companion object {
-        fun formatDate(date: Date) : String {
+        fun formatDate(date: Date): String {
             val format = SimpleDateFormat("dd/MM/yyy")
             return format.format(date)
         }
 
         // TODO: Other date formatting util methods here...
+
+        fun formatDateWithDay(date: Date): String {
+            val format = SimpleDateFormat("E dd/MM/yyy")
+            return format.format(date)
+        }
     }
 }
 
